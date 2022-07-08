c0cc1c15b709d0c7deae39540ecd8ed0e0607b58
isaacpitwa <isaacpitwa256@gmail.com>
2022-07-08 18:14:24 +0300

    Add comment test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Comment.new(text: 'Lorem ipsum dolor sit amet', post: Post.first, user: User.first) }
+
+  before { subject.save }
+
+  it 'text should be present' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'text must not exceed 255 characters' do
+    subject.text = 'a' * 256
+    expect(subject).to_not be_valid
+  end
+
+  it 'user should be present' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'post should be present' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  describe '#update_comment_counter' do
+    it 'should increment the comments_counter of the post' do
+      comments_counter = subject.post.comments_counter
+      subject.update_comment_counter
+      expect(subject.post.comments_counter).to eq(comments_counter + 1)
+    end
+  end
 end
