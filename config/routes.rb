d3b80797c7b27ebe71fbd70e7ff1f0d53cd7c063
isaacpitwa <isaacpitwa256@gmail.com>
2022-07-23 10:40:28 +0300

    Add User role to record

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create,]
+    resources :comments, only: [:create, :destroy]
     post "/posts/:id/like" => "posts#like", as: :like
     post "/posts/:id/comment" => "posts#comment", as: :comments
   end
