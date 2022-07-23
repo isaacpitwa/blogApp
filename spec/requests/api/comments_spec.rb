795ef2a8e3df1d4b8e23a0acf539deab37496c14
isaacpitwa <isaacpitwa256@gmail.com>
2022-07-23 19:18:25 +0300

    generate swagger doc

---
+++ spec/requests/api/comments_spec.rb
@@ -0,0 +1,39 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/comments', type: :request do
+
+  path '/api/posts/{id}/comments' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('list comments') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create comment') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
